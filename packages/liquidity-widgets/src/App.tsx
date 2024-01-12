0b1b5f19f2f500fe14c0ed3df01c7276a3f5aca5
viet-nv <vietnv1304@gmail.com>
2024-01-12 10:15:04 +0700

    init

---
+++ packages/liquidity-widgets/src/App.tsx
@@ -0,0 +1,12 @@
+import { LiquidityWidget } from "./components";
+import { defaultTheme } from "./theme";
+
+function App() {
+  return (
+    <>
+      <LiquidityWidget theme={defaultTheme} />
+    </>
+  );
+}
+
+export default App;
