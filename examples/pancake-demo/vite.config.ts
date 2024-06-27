7a7d6df8de6bb31513455f4cd3546d295cbc4c93
Hung Doan <hungdoansy@gmail.com>
2024-06-27 16:53:41 +0700

    feat: impl demo

---
+++ examples/pancake-demo/vite.config.ts
@@ -0,0 +1,42 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import nodePolyfills from "rollup-plugin-polyfill-node";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    global: "globalThis",
+  },
+  build: {
+    rollupOptions: {
+      plugins: [nodePolyfills()],
+    },
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+    target: ["esnext"], // 👈 build.target
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: "esnext",
+      // Node.js global to browser globalThis
+      define: {
+        global: "globalThis",
+      },
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+        }),
+      ],
+      supported: {
+        bigint: true,
+      },
+    },
+    // Source: https://stackoverflow.com/a/75953479/6812545
+    // This helps resolve 504 (Outdated Optimize Dep)
+    exclude: ["js-big-decimal"],
+  },
+});
