c4cc19454f859133510f1e62f1d5a20a057fa5d3
Tien Nguyen <dinhtiennguyen.1202@gmail.com>
2024-10-03 21:56:03 +0700

    Fix demo container styles

--- apps/liquidity-widgets-demo/src/App.tsx
+++ apps/liquidity-widgets-demo/src/App.tsx
@@ -92,51 +92,57 @@ function App() {
 
   return (
     <>
-      <button
-        style={{ color: "#fff" }}
-        onClick={() => (wallet ? disconnect(wallet) : connect())}
-      >
-        {!wallet ? "Connect wallet" : "Disconnect"}
-      </button>
-      <div>{wallet?.accounts?.[0].address}</div>
-      <LiquidityWidget
-        // theme={{
-        //   text: "#FFFFFF",
-        //   subText: "#B6AECF",
-        //   icons: "#a9a9a9",
-        //   layer1: "#27262C",
-        //   dialog: "#27262C",
-        //   layer2: "#363046",
-        //   stroke: "#363046",
-        //   chartRange: "#5DC5D2",
-        //   chartArea: "#457F89",
-        //   accent: "#5DC5D2",
-        //   warning: "#F4B452",
-        //   error: "#FF5353",
-        //   success: "#189470",
-        //   fontFamily: "Kanit, Sans-serif",
-        //   borderRadius: "20px",
-        //   buttonRadius: "16px",
-        //   boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.04)",
-        // }}
-        provider={ethersProvider}
-        chainId={42161}
-        // positionId="730708"
-        poolType={PoolType.DEX_UNISWAPV3}
-        poolAddress="0x641C00A822e8b671738d32a431a4Fb6074E5c79d"
-        // chainId={56}
-        // positionId="24654"
-        // poolType={PoolType.DEX_PANCAKESWAPV3}
-        // poolAddress="0x36696169c63e42cd08ce11f5deebbcebae652050"
-        // feeAddress="0xB82bb6Ce9A249076Ca7135470e7CA634806De168"
-        // feePcm={50}
-        onDismiss={() => {
-          window.location.reload();
-        }}
-        source="zap-widget-demo"
-        // initDepositTokens="0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9"
-        // initAmounts="1"
-      />
+      <div className="demo-support-container">
+        <button
+          className="demo-btn-wallet"
+          onClick={() => (wallet ? disconnect(wallet) : connect())}
+        >
+          {!wallet ? "Connect wallet" : "Disconnect"}
+        </button>
+        <div className="demo-wallet-address">
+          {wallet?.accounts?.[0].address}
+        </div>
+      </div>
+      <div className="demo-widget-wrapper">
+        <LiquidityWidget
+          // theme={{
+          //   text: "#FFFFFF",
+          //   subText: "#B6AECF",
+          //   icons: "#a9a9a9",
+          //   layer1: "#27262C",
+          //   dialog: "#27262C",
+          //   layer2: "#363046",
+          //   stroke: "#363046",
+          //   chartRange: "#5DC5D2",
+          //   chartArea: "#457F89",
+          //   accent: "#5DC5D2",
+          //   warning: "#F4B452",
+          //   error: "#FF5353",
+          //   success: "#189470",
+          //   fontFamily: "Kanit, Sans-serif",
+          //   borderRadius: "20px",
+          //   buttonRadius: "16px",
+          //   boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.04)",
+          // }}
+          provider={ethersProvider}
+          chainId={42161}
+          // positionId="730708"
+          poolType={PoolType.DEX_UNISWAPV3}
+          poolAddress="0x641C00A822e8b671738d32a431a4Fb6074E5c79d"
+          // chainId={56}
+          // positionId="24654"
+          // poolType={PoolType.DEX_PANCAKESWAPV3}
+          // poolAddress="0x36696169c63e42cd08ce11f5deebbcebae652050"
+          // feeAddress="0xB82bb6Ce9A249076Ca7135470e7CA634806De168"
+          // feePcm={50}
+          onDismiss={() => {
+            window.location.reload();
+          }}
+          source="zap-widget-demo"
+          // initDepositTokens="0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9"
+          // initAmounts="1"
+        />
+      </div>
     </>
   );
 }
