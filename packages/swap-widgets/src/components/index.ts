f2bb067d0629221bfb954b322c5c67b109b89f24
viet-nv <vietnv1304@gmail.com>
2024-10-14 00:25:00 +0700

    feat: sentry integration

--- packages/swap-widgets/src/components/index.ts
+++ packages/swap-widgets/src/components/index.ts
@@ -1,2 +1,22 @@
 export { default as Widget } from './Widget'
 export * from './Widget'
+
+import * as Sentry from '@sentry/react'
+
+Sentry.init({
+  dsn: 'https://50dafb1c10e301e6d4821bff21520cc4@sentry.ops.kyberengineering.io/7',
+  integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
+  ignoreErrors: ['AbortError'],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for tracing.
+  tracesSampleRate: 1.0,
+
+  // Set `tracePropagationTargets` to control for which URLs trace propagation should be enabled
+  // tracePropagationTargets: [/^\//, /^https:\/\/yourserver\.io\/api/],
+
+  // Capture Replay for 10% of all sessions,
+  // plus for 100% of sessions with an error
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+})
