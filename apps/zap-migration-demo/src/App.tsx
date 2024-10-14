c9c685a3de1a56d4a40e195fa39699c13e14579c
viet-nv <vietnv1304@gmail.com>
2024-10-14 16:33:57 +0700

    tmp

---
+++ apps/zap-migration-demo/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { ZapMigration } from "@kyber/zap-migration-widgets";
+import "@kyber/zap-migration-widgets/dist/style.css";
+
+function App() {
+  return (
+    <>
+      <ZapMigration />
+    </>
+  );
+}
+
+export default App;
---
+++ packages/zap-migration-widgets/src/index.tsx
@@ -0,0 +1,5 @@
+import "./index.css";
+
+export function ZapMigration() {
+  return <div className="p-10">Migration</div>;
+}
