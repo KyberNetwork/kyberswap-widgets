36d7a51e1cf8e87598d87b14e0edb7fa70a0fcfc
Tien Nguyen <dinhtiennguyen.1202@gmail.com>
2025-03-12 00:20:48 +0700

    chore: add type for svg

--- apps/zap-widgets-demo/src/vite-env.d.ts
+++ apps/zap-widgets-demo/src/vite-env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="vite/client" />
+
+declare module "*.svg?react" {
+  const content: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  export default content;
+}
--- packages/ui/src/svg.d.ts
+++ packages/ui/src/svg.d.ts
@@ -2,3 +2,7 @@ declare module "*.svg" {
   const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
   export default content;
 }
+declare module "*.svg?react" {
+  const content: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  export default content;
+}
